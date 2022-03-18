9977c054771823afedca867bc3fa3f4b87b68c5e
Максим Корман <mkorman359@gmail.com>
2022-03-18 15:57:04 +0200

    Add project files.

---
+++ Lab_1_ООП_Корман_М_Б/Lab_1_ООП_Корман_М_Б.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int L_name = 30;
+
+class GOODS
+{
+	private:
+		char name[L_name];
+		int number;
+		float price;
+	public:
+		
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "ukr");
+}
